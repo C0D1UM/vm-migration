323b0189474078c18390ae82bee2f0e4a56c9d85
Htet Naing Aung <htet_naing.a@codium.co>
2018-02-16 17:51:01 +0700

    add docker-compose installation

--- scripts/install_pg_nginx_docker.sh
+++ scripts/install_pg_nginx_docker.sh
@@ -20,3 +20,5 @@ sudo add-apt-repository \
    stable"
 sudo apt-get update
 sudo apt-get install -y docker-ce
+sudo curl -L https://github.com/docker/compose/releases/download/1.19.0/docker-compose-`uname -s`-`uname -m` -o /usr/local/bin/docker-compose
+sudo chmod +x /usr/local/bin/docker-compose
