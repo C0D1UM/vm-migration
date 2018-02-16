b54729aaf293da559b18061d36002ff8a053076a
Htet Naing Aung <htet_naing.a@codium.co>
2018-02-16 17:51:18 +0700

    format docker/config.json

--- scripts/setup_aws_ecr_login.sh
+++ scripts/setup_aws_ecr_login.sh
@@ -1,11 +1,10 @@
 # Config docker login
 mkdir -p ~/.docker
 mkdir -p ~/.aws
-echo '{"credsStore": "ecr-login"}' > ~/.docker/config.json
+echo '{"\n  credsStore": "ecr-login"\n}' > ~/.docker/config.json
 
 # Install docker-credential-ecr-login helper
 chmod +x ~/docker-credential-ecr-login
 sudo mkdir -p /opt/bin/
-sudo mv ~/docker-credential-ecr-login /opt/bin/ 
+sudo mv ~/docker-credential-ecr-login /opt/bin/
 echo export PATH=$PATH:/opt/bin > /etc/profile
-echo 'Successful docker-credential-ecr-login helper installation'
