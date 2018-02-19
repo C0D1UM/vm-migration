4b7a08e9ceb471b2f6ee9ccb3eecb7cfc637b6af
Htet Naing Aung <htet_naing.a@codium.co>
2018-02-19 17:06:05 +0700

    update postgres client 10 installation script

--- scripts/install_pg_client_10_1704.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-
-# reference: https://gist.github.com/alistairewj/8aaea0261fe4015333ddf8bed5fe91f8
-sudo add-apt-repository 'deb http://apt.postgresql.org/pub/repos/apt/ zesty-pgdg main'
-wget --quiet -O - https://www.postgresql.org/media/keys/ACCC4CF8.asc | \
-  sudo apt-key add -
-sudo apt-get install -y postgresql-client-10 postgresql-client-common
