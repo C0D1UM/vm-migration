771ef3aad9994431c39e005932bf4cb9fba2711c
Htet Naing Aung <htet_naing.a@codium.co>
2018-02-19 19:16:22 +0700

    add -y option in install postgres client 10

--- scripts/install_pg_client_10.sh
+++ scripts/install_pg_client_10.sh
@@ -2,7 +2,7 @@
 
 # reference: https://wiki.postgresql.org/wiki/Apt 
 echo "deb http://apt.postgresql.org/pub/repos/apt/ $(lsb_release -cs)-pgdg main" > /etc/apt/sources.list.d/pgdg.list
-sudo apt-get install wget ca-certificates
+sudo apt-get install -y wget ca-certificates
 wget --quiet -O - https://www.postgresql.org/media/keys/ACCC4CF8.asc | sudo apt-key add -
 sudo apt-get update
 sudo apt-get install -y postgresql-client-10
