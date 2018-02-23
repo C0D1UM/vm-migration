ecca938c12e6fdb41a58f93725db900c74954e02
Piyapan Poomsirivilai <piyapan039285@gmail.com>
2018-02-23 19:12:44 +0700

    Update setup_aws_ecr_login.sh
    Changes
    - Add AWS setup instructions

--- scripts/setup_aws_ecr_login.sh
+++ scripts/setup_aws_ecr_login.sh
@@ -5,6 +5,11 @@ mkdir -p ~/.docker
 mkdir -p ~/.aws
 echo '{"credsStore": "ecr-login"}' > ~/.docker/config.json
 
+#you have to create file ~/.aws/credentials manually. File content will be 
+# [default]
+# aws_access_key_id=<your key>
+# aws_secret_access_key=<your key>
+
 # Install docker-credential-ecr-login helper
 chmod +x ~/docker-credential-ecr-login
 sudo mkdir -p /opt/bin/
