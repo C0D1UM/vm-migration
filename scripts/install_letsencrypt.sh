640b86d1a83cd08de9d15d962b47dddeb62287b1
Piyapan Poomsirivilai <piyapan039285@gmail.com>
2018-02-23 13:55:01 +0700

    Update install_letsencrypt.sh
    Changes
    - Add command according to [Official website](https://certbot.eff.org/#ubuntuxenial-other)

--- scripts/install_letsencrypt.sh
+++ scripts/install_letsencrypt.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 
+apt-get update
+sudo apt-get install software-properties-common
 sudo add-apt-repository ppa:certbot/certbot
-apt update
+apt-get update
 apt install -y certbot
